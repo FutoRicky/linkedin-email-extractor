596e9b2529471e12bc5bf9e3124d91d3d81b0644
Ricardo Mercado <ricardoamercado@gmail.com>
2018-12-22 19:54:38 -0400

    Add interval emails functionality

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 const csv = require('fast-csv');
 const prompt = require('prompt')
 const Nightmare = require('nightmare')
-const nightmare = Nightmare({ 
-  show: true,
+let nightmare = Nightmare({ 
   waitTimeout: 20000
 })
 
@@ -16,31 +15,55 @@ csv
      connections.push(`${data['First Name']} ${data['Last Name']}`);
  })
  .on("end", function(){
-     console.log("done");
-    //  console.log(connections)
     start();
  });
 
 let prompt_attrs = [
-  { name: 'email' },
-  { name: 'password', hidden: true }
+  { 
+    name: 'email', 
+    required: true, 
+    message:"LinkedIn email" 
+  },
+  { 
+    name: 'password', 
+    hidden: true, 
+    required: true, 
+    message:"LinkedIn password" 
+  },
+  { 
+    name: 'interval', 
+    message: 'How frequent should we get emails? (In milliseconds, 1 hour = 3600000)',
+    default: '3600000'   
+  },
+  { 
+    name: 'limit', 
+    message: 'How many emails per interval?',
+    default: '25'
+  }
 ]
+
+let email, password, interval, limit;
+let emails = [];
+let index = 0;
+let intervalSet = false;
 
 function start() {
   prompt.start()
   
   prompt.get(prompt_attrs, (err, result) => {
-    let email = result.email
-    let password = result.password
-
-    getEmails(email, password, 0);
+    email = result.email
+    password = result.password
+    interval = parseInt(result.interval)
+    limit = parseInt(result['emails per interval'])
   })
+
+  getEmails(index);
 }
 
 
 let result = []
 
-async function getEmails(email, password, index) {
+async function getEmails(index) {
   try {
     await nightmare
     .goto('https://linkedin.com')
@@ -49,15 +72,17 @@ async function getEmails(email, password, index) {
     .click('#login-submit')
     .wait('.nav-search-typeahead')
     .run(() => {
-      getEmail(email, password, index);
+      getEmail(index);
     })
   } catch(e) {
     console.error(e);
   }
 }
 
-async function getEmail(email, password, index) {
-  if (index < connections.length) {
+async function getEmail(index, count) {
+  count = count || 0;
+
+  if (count < limit) {
     try {
       await nightmare
       .wait('.nav-search-typeahead')
@@ -70,7 +95,7 @@ async function getEmail(email, password, index) {
       .wait('.pv-top-card-v2-section__link--contact-info')
       .click('.pv-top-card-v2-section__link--contact-info')
       .wait('.pv-contact-info.artdeco-container-card')
-      .wait(3000)
+      .wait(2000)
 
       result.push(
         await nightmare
@@ -83,18 +108,43 @@ async function getEmail(email, password, index) {
   } else {
     await nightmare
     .end();
-    console.log(result)
+    addEmailsToFile(result)
+
+    if (!intervalSet) {
+      intervalSet = true;
+      setInterval(function() {
+        if (result.length >= 4) {
+        // if (result.length >= connections.length) {
+          console.log("Email extraction complete. You can end this program pressing Ctrl+C");
+        } else {
+          nightmare = Nightmare({ 
+            waitTimeout: 20000
+          })
+          getEmails(result.length);
+        }
+      }, interval)
+    }
   }
   try {
     const result = await nightmare
       .evaluate(() => { return document.querySelector('.pv-contact-info__contact-type.ci-email a.pv-contact-info__contact-link').href.replace('mailto:', ''); })
       .run((result) => {
-        console.log(result);
-        getEmail(email, password, index + 1)
+        count++;
+        getEmail(index + 1, count)
       })
-    console.log(result);
+    
+    emails.push(result);
+
   } catch(e) {
     console.error(e);
     return undefined;
   }
+}
+
+function addEmailsToFile(data) {
+  fs.writeFile('emails.txt', data, function(err) { 
+    if (err) throw err;
+    // if no error
+    console.log(`${limit} new email(s) added. ${result.length} email(s) in file.`)
+  });
 }
