017dc2057fdd61a94d664d90aec4c972afb4fbb7
Ricardo <ricardoamercado@gmail.com>
2018-12-26 09:34:24 -0400

    Successfull email extraction

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const fs = require('fs');
 const csv = require('fast-csv');
-const prompt = require('prompt')
-const Nightmare = require('nightmare')
-let nightmare = Nightmare({ 
-  waitTimeout: 20000
-})
+const prompt = require('prompt');
+const Nightmare = require('nightmare');
 
 let connections = [];
+let nightmare;
 
 // Get connection names from connections.csv
-let stream = fs.createReadStream("connections.csv");
+let stream = fs.createReadStream("Connections.csv");
 csv
  .fromStream(stream, {headers : true})
  .on("data", function(data){
@@ -17,6 +15,7 @@ csv
  })
  .on("end", function(){
     //  After connection names are setup, start email extraction process
+    console.log("Total connections: ", connections.length)
     start();
  });
 
@@ -25,48 +24,43 @@ let prompt_attrs = [
   { 
     name: 'email', 
     required: true, 
-    message:"LinkedIn email" 
+    message: "LinkedIn email" 
   },
   { 
     name: 'password', 
     hidden: true, 
     required: true, 
-    message:"LinkedIn password" 
+    message: "LinkedIn password" 
   },
-  { 
-    name: 'interval', 
-    message: 'How frequent should we get emails? (In milliseconds, 1 hour = 3600000)',
-    default: '3600000'   
-  },
-  { 
-    name: 'limit', 
-    message: 'How many emails per interval?',
-    default: '25'
+  {
+    name: 'showNightmare',
+    default: "no",
+    message: "Show email extraction process? (yes/no)"
   }
 ]
 
 // Define variables
-let email, password, interval, limit;
+let email, password, showNightmare;
 let emails = [];
 let index = 0;
-let intervalSet = false;
 
 // This function starts the process by asking user for LinkedIn credentials, as well config options
 // - email & password are used to log in to linkedin
-// - interval establishes how often will emails be extracted
-// - limit is the amount of emails to extract on every interval
 function start() {
   prompt.start()
   
   prompt.get(prompt_attrs, (err, result) => {
     email = result.email
     password = result.password
-    interval = parseInt(result.interval)
-    limit = parseInt(result['emails per interval'])
+    showNightmare = result.showNightmare === "yes" ? true : false
+    nightmare = Nightmare({
+      show: showNightmare,
+      waitTimeout: 20000
+    })
+    getEmails(index);
   })
-
-  getEmails(index);
 }
+
 
 // Emails are stored in this array to be written to email.txt later.
 let result = []
@@ -77,15 +71,15 @@ async function getEmails(index) {
   try {
     await nightmare
     .goto('https://linkedin.com')
-    .type('#login-email', email)
-    .type('#login-password', password)
+    .insert('#login-email', email)
+    .insert('#login-password', password)
     .click('#login-submit')
-    .wait('.nav-search-typeahead')
+    .wait('.nav-item--mynetwork')
     .run(() => {
       getEmail(index);
     })
   } catch(e) {
-    console.error(e);
+    console.error("An error occured while attempting to login to linkedin.")
   }
 }
 
@@ -95,67 +89,64 @@ async function getEmail(index, count) {
   count = count || 0;
 
   // Condition is here to make sure no more than the limit of emails is extracted on each interval
-  if (count < limit) {
+  if (count < connections.length) {
     try {
       await nightmare
-      .wait('.nav-search-typeahead')
-      .click('.nav-search-typeahead')
-      .type('.nav-search-typeahead input', "")
-      .type('.nav-search-typeahead input', connections[index])
-      .type('.nav-search-typeahead input', '\u000d')
-      .wait('.search-results__list')
-      .click('.search-result__result-link')
+      .wait('.nav-item--mynetwork')
+      .click('.nav-item--mynetwork a')
+      .wait('.js-mn-origami-rail-card__connection-count')
+      .click('.js-mn-origami-rail-card__connection-count')
+      .wait('.mn-connections__search-input')
+      .insert('.mn-connections__search-input', connections[index])
+      .wait(2000)
+      .click('.mn-connection-card__link')
       .wait('.pv-top-card-v2-section__link--contact-info')
       .click('.pv-top-card-v2-section__link--contact-info')
       .wait('.pv-contact-info.artdeco-container-card')
-      .wait(2000)
 
       result.push(
         await nightmare
+
         // here we get the email from the connections linkedin page.
-        .evaluate(() => { return document.querySelector('.pv-contact-info__contact-type.ci-email a.pv-contact-info__contact-link').href.replace('mailto:', ''); })
+        .evaluate(() => {
+          try {
+            return document.querySelector('.pv-contact-info__contact-type.ci-email a.pv-contact-info__contact-link').href.replace('mailto:', ''); 
+          } catch(e) {
+            console.error("An email could not be extracted.")
+          }
+        })
       )
 
     } catch(e) {
-      console.error(e);
+      console.error("Unable to extract email from connection # ", count);
     }
   } else {
-    // If interval email limit is reached, end the crawler session and add emails to emails.txt
-    // Then start the interval if it has not been set already
+    // When all emails have been extracted, end nightmare crawler and add emails to email.txt
     await nightmare
     .end();
     addEmailsToFile(result)
-
-    if (!intervalSet) {
-      intervalSet = true;
-      setInterval(function() {
-
-        // If result.length is equal to connections.length then email extraction is completed.
-        // Let user know the process has ended.
-        // If process has not ended, continue with procedure interval.
-        if (result.length >= connections.length) {
-          console.log("Email extraction complete. You can end this program pressing Ctrl+C");
-        } else {
-          nightmare = Nightmare({ 
-            waitTimeout: 20000
-          })
-          getEmails(result.length);
-        }
-      }, interval)
-    }
+    return
   }
   try {
     const result = await nightmare
-      .evaluate(() => { return document.querySelector('.pv-contact-info__contact-type.ci-email a.pv-contact-info__contact-link').href.replace('mailto:', ''); })
+      .evaluate(() => {
+        try {
+          return document.querySelector('.pv-contact-info__contact-type.ci-email a.pv-contact-info__contact-link').href.replace('mailto:', ''); 
+        } catch(e) {
+          console.log("An email could not be extracted.");
+          return undefined
+        }
+      })
       .run((result) => {
         count++;
+        console.log("#", count)
         getEmail(index + 1, count)
       })
     
     emails.push(result);
 
   } catch(e) {
-    console.error(e);
+    console.error('An email could not be extracted.');
     return undefined;
   }
 }
@@ -165,6 +156,6 @@ function addEmailsToFile(data) {
   fs.writeFile('emails.txt', data, function(err) { 
     if (err) throw err;
     // if no error
-    console.log(`${limit} new email(s) added. ${result.length} email(s) in file.`)
+    console.log(`${result.length} email(s) extracted.`)
   });
 }
